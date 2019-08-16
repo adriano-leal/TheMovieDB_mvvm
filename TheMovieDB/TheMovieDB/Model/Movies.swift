675664cb907904434b835226f034de8ca961e4ae
Adriano Leal Ramos <ramos.adrianoleal@gmail.com>
2019-08-16 11:19:30 -0300

    network layer 2

--- TheMovieDB/TheMovieDB/Model/Movies.swift
+++ TheMovieDB/TheMovieDB/Model/Movies.swift
@@ -4,16 +4,45 @@
 //
 //  Created by Adriano Ramos on 15/08/19.
 //  Copyright © 2019 Adriano Ramos. All rights reserved.
-//
+
+// This file was generated from JSON Schema using quicktype, do not modify it directly.
+// To parse the JSON, add this file to your project and do:
+//   let Movies = try? newJSONDecoder().decode(Movies.self, from: jsonData)
 
 import Foundation
 
-
-struct Movies: Codable {
-    var adult: Bool
+// MARK: - PopularMovie
+struct Result: Codable {
+    let page, totalResults, totalPages: Int
+    let results: [Movie]
     
     enum CodingKeys: String, CodingKey {
-        case adult
+        case page
+        case totalResults = "total_results"
+        case totalPages = "total_pages"
+        case results
     }
+}
+
+// MARK: - Result
+struct Movie: Codable {
+    let id: Int
+    let voteAverage: Double
+    let title: String
+    let posterPath: String
+    let originalTitle: String
+    let genreIDS: [Int]
+    let backdropPath: String
+    let overview: String
     
+    enum CodingKeys: String, CodingKey {
+        case id
+        case voteAverage = "vote_average"
+        case title
+        case posterPath = "poster_path"
+        case originalTitle = "original_title"
+        case genreIDS = "genre_ids"
+        case backdropPath = "backdrop_path"
+        case overview
+    }
 }
---
+++ TheMovieDB/TheMovieDB/Networking/NetworkManager.swift
@@ -0,0 +1,18 @@
+//
+//  NetworkManager.swift
+//  TheMovieDB
+//
+//  Created by Adriano Ramos on 16/08/19.
+//  Copyright © 2019 Adriano Ramos. All rights reserved.
+//
+
+import Foundation
+
+class NetworkManager {
+    
+    
+    static let shared = NetworkManager()
+    
+    
+    
+}
